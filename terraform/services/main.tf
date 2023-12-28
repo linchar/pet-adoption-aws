77e9ab46e5ee1e9d65ca05eb37c7c241a8160859
linchar <linchar@gmail.com>
2023-12-28 23:12:48 +0000

    move locals block to main

--- terraform/services/main.tf
+++ terraform/services/main.tf
@@ -9,5 +9,21 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
+  region = local.region
+}
+
+locals {
+  name   = "petadoption-workshop"
   region = "us-east-1"
+
+  vpc_cidr = "11.0.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"]
+
+  public_subnets  = ["11.0.1.0/24", "11.0.2.0/24"]
+  private_subnets = ["11.0.3.0/24", "11.0.4.0/24"]
+  intra_subnets   = ["11.0.5.0/24", "11.0.6.0/24"]
+
+  tags = {
+    Workshop = true
+  }
 }
--- terraform/services/resource.tf
+++ terraform/services/resource.tf
@@ -14,22 +14,6 @@ From cdk, uses the current ec2 intances's cidr range
             maxAzs: 2
         });
 */
-locals {
-  name   = "petadoption-workshop"
-  region = "us-east-1"
-
-  vpc_cidr = "11.0.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
-
-  public_subnets  = ["11.0.1.0/24", "11.0.2.0/24"]
-  private_subnets = ["11.0.3.0/24", "11.0.4.0/24"]
-  intra_subnets   = ["11.0.5.0/24", "11.0.6.0/24"]
-
-  tags = {
-    Workshop = true
-  }
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.4"
@@ -56,9 +40,7 @@ module "vpc" {
 // Create SQS resource to send Pet adoption messages to
 resource "aws_sqs_queue" "sqspetadoption" {
   visibility_timeout_seconds = 300
-  tags = {
-    Workshop = "true"
-  }
+  tags = local.tags
 }
 
 // Create SNS and an email topic to send notifications to
